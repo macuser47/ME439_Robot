d543f9b180d58f86e7198904b51a95b1dc1cb86b
pi <>
2022-02-20 20:52:13 -0600

    Remove unnecessary comments

--- src/basic_motors_and_sensors/src/sensors_node.py
+++ src/basic_motors_and_sensors/src/sensors_node.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-title: sensors_node Node - ME439 Intro to robotics, wisc.edu
-Author: Peter Adamczyk 
-Updated 2021-09-21
-
-sensors_node.py
-ROS Node to a continually read data from a Serial port ('/dev/ttyUSB0' (Arduino Nano on USB) and publish it on a topic ("sensors_A0" or other)   
+ROS Node to a continually read data from a Serial port ('/dev/ttyUSB0' (Arduino
+Nano on USB) and publish it to relevant topics 
 """
 
 
 import rospy
-
-# Import "serial" to get data from the AlaMode
 import serial
-
-# "traceback" is a library that lets you track down errors.
 import traceback
-
-# Import the message types we will need
 from std_msgs.msg import Int32
 
 
@@ -26,38 +16,19 @@ from std_msgs.msg import Int32
 # Here we publish:
 #    Analog value of interest (levels)
 #
-# Could also publish Encoders (E0 and E1, (in "counts")), more Analogs (A0-A7, in "levels" 0-1023) and Ultrasound (in microseconds till echo) readings
 # Here we publish each in a separate topic.
 # In the long-term it would be better to publish them together.
 
-# This is the central code: set up a Node, set up a Publisher(s)
+
 def sensors_reader():
-    # Launch a node called "sensors_node"
     rospy.init_node("sensors_node", anonymous=False)
 
-    # Create the publishers. Name each topic "sensors_##", with message type "Int32"
-    # (because that's what is received over the serial port)
-    # Note the queue_size=1 statement: don't let it develop a backlog!
-
-    # Example: A0
+    # Create a publisher for each sensor
     pub_A0 = rospy.Publisher("/light_dark", Int32, queue_size=1)
     pub_A1 = rospy.Publisher("/ultrasonic", Int32, queue_size=1)
 
-    # Other examples:
-    # pub_E0 = rospy.Publisher('/sensors_E0', Int32, queue_size=1)
-    # pub_E1 = rospy.Publisher('/sensors_E1', Int32, queue_size=1)
-
-    # Declare the message that will go on the topic.
-    # Here we use one of the message name types we Imported, and add parentheses to call it as a function.
-    # We put data in it using the .data field of the message.
     msg_A0 = Int32()
     msg_A1 = Int32()
-
-    # Other examples
-    #    msg_E0 = Int32()
-    #    msg_E1 = Int32()
-
-    # Data comes in on the Serial port. Set that up and start it.
 
     # ----------setup serial--------------
     ser = serial.Serial(
@@ -72,24 +43,16 @@ def sensors_reader():
     ser.flush()  # Flush any data currently on the port
     ser.readline()
 
-    # MAIN LOOP to keep loading the message with new data.
-    # NOTE that at the moment the data are coming from a separate thread, but this will be replaced with the serial port line reader in the future.
     while not rospy.is_shutdown():
 
         try:
-            # Here we read the serial port for a string that looks like "e0:123456", which is an Encoder0 reading.
-            # When we get a reading, update the associated motor command
-            line = (
-                ser.readline().decode().strip()
-            )  # blocking function, will wait until read entire line
-            #            print(line)
+            # Read the serial port for a string that looks like "e0:123456",
+            # with format [INPUT NAME]:[VALUE]
+            line = ser.readline().decode().strip()
             line = line.split(":")
-            # Element 0 of "line" will be a string that says what the data are:
             data_type = line[0]
-            # Element 1 of "line" will be the value, an Integer
             data_value = int(line[1])
-            #            print(data_type)
-            #            print(line)
+
             if data_type == "A0":
                 msg_A0 = data_value  # Analog reading
                 pub_A0.publish(msg_A0)
--- src/basic_motors_and_sensors/src/sensors_processor.py
+++ src/basic_motors_and_sensors/src/sensors_processor.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-title: sensors_processor node - ME439 Intro to robotics, wisc.edu
-Author: Peter Adamczyk 
-Updated 2021-09-21
-
-sensors_processor.py
-ROS Node to a receive sensor data on a topic ("sensors_A0" or other) and process it, then publish the processed version to another topic ("sensors_A0_processed" or similar)  
+ROS Node to a receive sensor data on raw sensor data topics and process it,
+then publish the processed version to another topic
 """
 
 
@@ -14,31 +10,26 @@ import rospy
 from std_msgs.msg import Int32, Int32MultiArray, Float32, Bool, Float32MultiArray
 import numpy as np
 
-# First create one or more publishers for the topics that will hold the processed data.
-# They are created outside the "listener" function so that they are "in scope" (i.e. readable) when they are eventually used within the Callback function(s) invoked by the Subscriber(s) upon receipt of incoming messages (here the callback is "cal_and_pub_A0")
+# Processed data publishers
 pub_A0_proc = rospy.Publisher("/light_dark_proc", Float32, queue_size=1)
 pub_A1_proc = rospy.Publisher("/ultrasonic_proc", Float32, queue_size=1)
-
 pub_enc_proc = rospy.Publisher("/encoder_proc", Float32MultiArray, queue_size=1)
 
 
-# This is the central code that will run in this node: setting up a node with a Subscriber object and associated Callback.
 def sensors_listener():
     # Initialize the Node
     rospy.init_node("sensors_processor", anonymous=False)
 
-    # Set up a listener for the topic you want
-    # Remember to name a "callback" function (must define below) to handle the data
+    # Listeners for sensor data
     sub_A0 = rospy.Subscriber("/light_dark", Int32, cal_and_pub_A0)
     sub_A1 = rospy.Subscriber("/ultrasonic", Int32, cal_and_pub_A1)
     sub_enc = rospy.Subscriber("/encoder", Int32MultiArray, cal_and_pub_encoder)
 
-    # spin() to prevent the function from exiting
     rospy.spin()
 
 
-# Callback function, which will be called with incoming message data when messages are received by the Subscriber above.
 def cal_and_pub_A0(msg_in):
+    """Compute and publish light/dark state analog reading"""
     analog_level = float(msg_in.data)
     if analog_level < 500:
         pub_A0_proc.publish(Float32(data=1))
@@ -47,28 +38,21 @@ def cal_and_pub_A0(msg_in):
 
 
 def cal_and_pub_A1(msg_in):
+    """Compute and publish ultrasonic distance from analog reading"""
     analog_level = float(msg_in.data)
 
-    # calibrate the relationship between the signal that comes in and the signal in real units
-    # This can be from a Data Sheet, or from measurements you make yourself.
-
-    # Example: MaxBotix LV-EZ02 says:
+    # MaxBotix LV-EZ02:
     # Distance in Inches = Volts / (Vcc/512).
     # For A0, Vcc is 3.3 Volts and signals are in 2^10 levels from 0 to Vcc
     # Therefore (Distance in Meters) = 0.0254 (m/in)*(distance in Inches)
 
-    ##### UPDATE THESE EQUATIONS
     analog_volts = analog_level * (3.3 / 1024)
     distance_meters = analog_volts / (3.3 / 512) * 0.0254
-    #####
 
-    # Create a Message that will hold the out-going data
+    # Create and publish distance message
     A1_proc = Float32()
-
-    # Pack the message with the processed data
     A1_proc.data = distance_meters
 
-    # Publish the newly packed Message
     pub_A1_proc.publish(A1_proc)
 
 
